937606b99f1daa0285e22a0ade8e5f13fb36ead8
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-01 21:57:56 +0530

    Update ProctectRoutes.jsx

--- frontend/src/utils/ProctectRoutes.jsx
+++ frontend/src/utils/ProctectRoutes.jsx
@@ -23,29 +23,4 @@ const ProctectRoutes = ({children,requireRole}) => {
 }
 
 export default ProctectRoutes
-// import { useEffect } from "react";
-// import { useAuth } from "../context/AuthContext";
-// import { useNavigate } from "react-router-dom";
 
-// const ProctectRoutes = ({ children, requireRole }) => {
-//   const { user } = useAuth();
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     if (!user) {
-//       navigate("/login");
-//       return;
-//     }
-//     if (!requireRole.includes(user.role)) {
-//       navigate("/Unauthorized");
-//       return;
-//     }
-//   }, [user, navigate, requireRole]);
-
-//   // If user is not logged in or role not allowed, render nothing
-//   if (!user || !requireRole.includes(user.role)) return null;
-
-//   return children;
-// };
-
-// export default ProctectRoutes;
