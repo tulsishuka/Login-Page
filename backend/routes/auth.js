a847ba72e3eca1c681f7f878fdc14efd664948ab
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-11 15:11:10 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,6 @@ import express from 'express';
 import login from '../controllers/authControllers.js';
 const router = express.Router();
 
-// Correct route
 router.post('/login', login);
 
 export default router;
